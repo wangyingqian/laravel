19cbaf478efca4b96709767517ff06fe0c3f726a
汪应乾 <891699279@qq.com>
2019-01-30 15:52:34 +0800

    优化

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,20 @@ namespace  App\Http\Controllers\Api;
 
 use App\Services\User\Contract\MemberInterface;
 use Illuminate\Http\Request;
+use Lawechat\WeChat;
 
 class UserController extends Controller
 {
     public function add(Request $request)
     {
+        $config = [
+            'app_id' => 'wx3cf0f39249eb0exx',
+            'secret' => 'f1c242f4f28f735d4687abb469072axx',
+
+        ];
+        $wechat = WeChat::make('official_account', $config);
+
+        return $wechat;
         return $this->make(MemberInterface::class)->create();
     }
 }
